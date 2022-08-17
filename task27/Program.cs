28770152db3f6a57e6816f6a92a64458e709ed8b
Наталья <109668015+andre19071@users.noreply.github.com>
2022-08-17 21:15:52 +0700

    task27

--- task27/Program.cs
+++ task27/Program.cs
@@ -2,3 +2,15 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int number  = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int res =0;
+
+for ( sum = 0; number > 0; sum+= res )
+{
+    res = number % 10;
+    number = number/10 ; 
+} 
+Console.WriteLine($"Сумма чисел равна = {sum}");
