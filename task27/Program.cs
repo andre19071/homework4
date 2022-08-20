40acabbfe4e032aa8df82bb31e5e255c11133eb6
Наталья <109668015+andre19071@users.noreply.github.com>
2022-08-20 12:32:11 +0700

    task27

--- task27/Program.cs
+++ task27/Program.cs
@@ -5,12 +5,12 @@
 
 Console.WriteLine("Введите число: ");
 int number  = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
+int sum =0;
 int res =0;
 
-for ( sum = 0; number > 0; sum+= res )
+for (sum=0; number>0;sum+=res)
 {
     res = number % 10;
-    number = number/10 ; 
+    number = number/10; 
 } 
-Console.WriteLine($"Сумма чисел равна = {sum}");
+Console.WriteLine($"Сумма цифр в числе равна {sum}");
